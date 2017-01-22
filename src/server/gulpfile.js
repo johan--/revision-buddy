b6867c2c0d09d03de69b0aa4a55476fa3dc459a3
Sharath Chandra <sharat@skiploop.com>
2017-01-22 18:31:31 +0530

    Updating the gulp and packages

---
+++ src/server/gulpfile.js
@@ -0,0 +1,39 @@
+﻿var tslint = require("gulp-tslint"),
+    gulp = require('gulp'),
+    tsc = require('gulp-typescript'),
+    tsProject = tsc.createProject('tsconfig.json'),
+    sourcemaps = require("gulp-sourcemaps");
+del = require('del');
+
+const TYPE_SCRIPT_FILES = ["src/server/**/*.ts"];
+const LIBRARY_TYPE_SCRIPT_DEFINITION = './typings/globals/**/*.ts';
+
+gulp.task('ts-lint', function () {
+    return gulp.src(TYPE_SCRIPT_FILES)
+        .pipe(tslint({
+            formatter: "prose"
+        }))
+        .pipe(tslint.report())
+});
+
+gulp.task('clean:out', function () {
+    return del([
+        'out/**/*'
+    ]);
+});
+
+/**
+ * Compile TypeScript and include references to library and app .d.ts files.
+ */
+gulp.task('compile-ts', function () {
+
+    return tsProject.src()
+        .pipe(tsProject())
+        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("out"))
+});
+
+
+gulp.task('default', ['clean:out', 'compile-ts']);
+gulp.task('ci', ['clean:out', 'ts-lint', 'compile-ts']);
