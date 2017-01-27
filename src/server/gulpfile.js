13d097037dbd9d835b25be34eb74e8f21a34ea62
Sharath Chandra <sharat@skiploop.com>
2017-01-27 17:04:56 +0530

    Updating the gulp to copy environment files and email-templates

--- src/server/gulpfile.js
+++ src/server/gulpfile.js
@@ -3,6 +3,7 @@
     tsc = require('gulp-typescript'),
     tsProject = tsc.createProject('tsconfig.json'),
     sourcemaps = require("gulp-sourcemaps");
+    
 del = require('del');
 
 const TYPE_SCRIPT_FILES = ["./**/*.ts"];
@@ -34,6 +35,14 @@ gulp.task('compile-ts', function () {
         .pipe(gulp.dest("out"))
 });
 
+gulp.task("copy-files", function () {
+    gulp.src('./email-templates/**/*/')
+        .pipe(gulp.dest('./out/email-templates'));
 
-gulp.task('default', ['clean:out', 'compile-ts']);
+    gulp.src('./.env.*')
+        .pipe(gulp.dest('./out/'));
+});
+
+
+gulp.task('default', ['clean:out', 'compile-ts', 'copy-files']);
 gulp.task('ci', ['clean:out', 'ts-lint', 'compile-ts']);
