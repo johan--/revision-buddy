0e86b552fadcb0c79799a400716a0e9707ae75ef
Sharath Chandra <sharat@skiploop.com>
2017-01-30 16:40:03 +0530

    Removing the copy of node_modules in to dist directory

--- src/server/gulpfile.js
+++ src/server/gulpfile.js
@@ -40,8 +40,8 @@ gulp.task("copy-files", function () {
     gulp.src('./email-templates/**/*')
         .pipe(gulp.dest('./out/services/email-templates'));
 
-    gulp.src('./node_modules/**/*')
-        .pipe(gulp.dest('./out/node_modules/'));
+    //gulp.src('./node_modules/**/*')
+    //    .pipe(gulp.dest('./out/node_modules/'));
 
     gulp.src('./.env.*')
         .pipe(gulp.dest('./out/'));
